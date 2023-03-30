a459184a00f2b9ca520b640420f04ed9c1fa374b
Datior <leonardowederveiga@yahoo.com>
2023-03-30 13:50:26 -0300

    Finalizado

--- src/componentes/colaborador/index.js
+++ src/componentes/colaborador/index.js
@@ -3,7 +3,7 @@ import './Colaborador.css'
 const colaborador = (props) => {
     return (
         <div className='colaborador'>
-            <div className='cabecalho'>
+            <div className='cabecalho' style={ {backgroundColor: props.corDeFundo} }>
                 <img src={props.img} alt={props.nome} />
             </div>
             <div className='rodape'>
